79551a860a479f2e8137cb373902a63905f8317d
maomao <yajunning@163.com>
2018-12-09 20:53:25 +0800

    将代码提交到本地

---
+++ git_git/src/com/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.git;
+
+/**
+ * 描述:
+ * ${DESCRIPTION}
+ *
+ * @author maomao
+ * @create 2018-12-09 20:43
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
